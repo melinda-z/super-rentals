0d1ec63c2c856cf6e2cd1730faee01d42fcb4b35
Melinda Zhang <melinda_hongye@yahoo.com>
2022-04-27 11:17:26 +0100

    generate rental component and tests

--- tests/integration/components/jumbo-test.js
+++ tests/integration/components/jumbo-test.js
@@ -6,11 +6,11 @@ import { hbs } from 'ember-cli-htmlbars';
 module('Integration | Component | jumbo', function (hooks) {
   setupRenderingTest(hooks);
 
-    test('it renders the content inside a jumbo header with a tomster', async function (assert) {
-      await render(hbs`<Jumbo>Hello World</Jumbo>`);
-    
-      assert.dom('.jumbo').exists();
-      assert.dom('.jumbo').hasText('Hello World');
-      assert.dom('.jumbo .tomster').exists();
+  test('it renders the content inside a jumbo header with a tomster', async function (assert) {
+    await render(hbs`<Jumbo>Hello World</Jumbo>`);
+
+    assert.dom('.jumbo').exists();
+    assert.dom('.jumbo').hasText('Hello World');
+    assert.dom('.jumbo .tomster').exists();
   });
 });
